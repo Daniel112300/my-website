b6ae19e5c4bd6671e5935bfd0d0fee35d72b4fa9
LIU MAN-CHU <98147449+manchuliu@users.noreply.github.com>
2025-11-14 04:39:47 +0800

    Delete smart_home_db.sql

--- smart_home_db.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-    d.device_name AS '設備名稱',
-    pl.energy_consumed AS '用電量(kWh)',
-    pl.cost AS '電費(元)'
-FROM power_logs pl
-JOIN devices d ON pl.device_id = d.device_id
-WHERE pl.log_date = CURDATE()
-ORDER BY pl.cost DESC;
